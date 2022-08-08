f70f559008bdd134512919bd12bf9704f08eb08d
PankajPatre <109396068+pankajpatre11@users.noreply.github.com>
2022-08-08 18:04:13 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -6,8 +6,8 @@ sudo mv nexus-3.41.0-01 nexus
 echo "fail1"
 sudo adduser nexus
 echo "nexus   ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/nexus
-sudo chown -R nexus:nexus /nexus
-sudo chown -R nexus:nexus /sonatype-work
+sudo chown -R nexus:nexus nexus
+sudo chown -R nexus:nexus sonatype-work
 echo "fail2"
 sed -i 's/#run_as_user=""/run_as_user="nexus"/' /nexus/bin/nexus.rc
 echo "fail3"
