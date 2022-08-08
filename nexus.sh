0b00be754b00cd8b4223525ea079bc0eee1ba3b5
PankajPatre <109396068+pankajpatre11@users.noreply.github.com>
2022-08-08 17:38:38 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,16 +1,14 @@
-apt-get update
-apt install openjdk-8-jdk -y
-java -version
-useradd -M -d /opt/nexus -s /bin/bash -r nexus
+sudo apt-get update
+sudo apt install openjdk-8-jre-headless
+sudo wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+tar -zxvf latest-unix.tar.gz
+sudo mv /nexus-3.30.1-01 /nexus
+sudo adduser nexus
 echo "nexus   ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/nexus
-wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
-mkdir /opt/nexus
-tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1ls /opt/nexus
-ls /opt/nexus
-chown -R nexus: /opt/nexus
+sudo chown -R nexus:nexus /nexus
+sudo chown -R nexus:nexus /sonatype-work
+sudo nano /nexus/bin/nexus.rc
 sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
-sudo -u nexus /opt/nexus/bin/nexus start
-tail -f /opt/nexus/sonatype-work/nexus3/log/nexus.log
 netstat -altnp | grep :8081
 cat > /etc/systemd/system/nexus.service << 'EOL'
 [Unit]
@@ -20,16 +18,17 @@ After=network.target
 [Service]
 Type=forking
 LimitNOFILE=65536
-ExecStart=/opt/nexus/bin/nexus start
-ExecStop=/opt/nexus/bin/nexus stop
+ExecStart=/nexus/bin/nexus start
+ExecStop=/nexus/bin/nexus stop
 User=nexus
 Restart=on-abort
 
 [Install]
 WantedBy=multi-user.target
 EOL
-/opt/nexus/bin/nexus stop
-systemctl daemon-reload
-systemctl enable --now nexus.service
-tail -f /opt/nexus/sonatype-work/nexus3/log/nexus.log
+sudo systemctl start nexus
+sudo systemctl enable nexus
+sudo systemctl status nexus
+sudo systemctl stop nexus
+tail -f /opt/sonatype-work/nexus3/log/nexus.log
 ufw allow 8081/tcp
